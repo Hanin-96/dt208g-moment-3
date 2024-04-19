57bc7b5b80937e74a7cf46e7d1ff665ee36718c1
Hanin Farhan <haninfarhan96@gmail.com>
2024-04-19 14:51:24 +0200

    exporterat alla routes, skapat komponenter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CalculateComponent } from './calculate/calculate.component';
+import { AboutComponent } from './about/about.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'calculate', component: CalculateComponent},
+    {path: 'about', component: AboutComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: '404', component: NotfoundComponent},
+    {path: '**', component: NotfoundComponent}
+];
