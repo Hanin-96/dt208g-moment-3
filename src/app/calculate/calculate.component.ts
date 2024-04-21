f8cc08ab218d5187ffdc53098808fd119dd13a4b
Hanin Farhan <haninfarhan96@gmail.com>
2024-04-21 16:28:09 +0200

    fixat favicon + ngModel & onchangemodel för temp input

--- src/app/calculate/calculate.component.ts
+++ src/app/calculate/calculate.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculate',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calculate.component.html',
   styleUrl: './calculate.component.css'
 })
 export class CalculateComponent {
+
+  tempCel: number = 0;
+  tempFah: number = 32;
+
+  onChangeTempCel(temp: number): void {
+    console.log(temp);
+    if (temp != null) {
+      this.tempFah = (temp * 1.8) + 32;
+    }
+  }
+
+  onChangeTempFah(temp: number): void {
+    if (temp != null) {
+      this.tempCel = (temp -32) * (5/9);
+    }
+  }
 
 }
